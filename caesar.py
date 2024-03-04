f3d5a0518f417758f11cd0e7e9cb572cdef11e44
Ted Pavlov <152305401+WafflesForDays@users.noreply.github.com>
2024-03-04 10:14:01 -0500

    Update caesar.py

--- caesar.py
+++ caesar.py
@@ -1,14 +1,25 @@
 #Ted Pavlov 348705732
 
-def keyShift(word, shift=3):
+def keyShift(word, shift):
+    shifted = []
     list(word)
     for i in word:
         i = ord(i)
-        i += shift
-        i = chr(i)
+        i += int(shift)
         
+        i = chr(i)
+        shifted.append(i)
+    shifted = "".join(shifted)
 
-    return word
+    return shifted
 
+#test cases:
+print(keyShift("hello", 3))
+print(keyShift("world", -1))
+print(keyShift("python", 5))
+print(keyShift("example", -2))
+print(keyShift("cryptography", -15))
 
-print(keyShift("test", 3))
+input = input()
+input = input.split(", ")
+print(keyShift(input[0], input[1]))
